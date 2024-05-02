f1e03e26aa8eaf2fe66993d9abb65950ecb3cd9c
Akshit Bhatt <akshitbhatt2011@gmail.com>
2024-05-02 22:57:17 +0530

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import{ Container, Logo , LogoutBtn} from '../index'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const authStatus=useSelector((state)=>
+    state.auth.status
+  )
+  const navigate=useNavigate()
+  const naItems=[
+    {
+      name:Home 
+    }
+  ]
+  return <div>Header</div>;
+}
+
+export default Header;
